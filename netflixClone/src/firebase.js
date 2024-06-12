8a33edec68f0690ff5dd86c3b3e3b50559d2a61f
Asrith Rajeevan <asrithrajeevan@Asriths-MacBook-Air.local>
2024-06-12 22:29:18 +0530

    finished netflix clone

---
+++ netflixClone/src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyAxPjjMsV1hglQp8KLx89oK2B0KkhV21jQ",
+    authDomain: "my-portfolio-1057c.firebaseapp.com",
+    projectId: "my-portfolio-1057c",
+    storageBucket: "my-portfolio-1057c.appspot.com",
+    messagingSenderId: "1073175595450",
+    appId: "1:1073175595450:web:8227c96c4b8d490394b583"
+};
+
